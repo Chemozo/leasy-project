74645d0ca348836812961a41718cd2b4471ec149
Chemozo <chemalebo@gmail.com>
2025-05-06 18:08:48 -0600

    added reports urls

--- core/reports/urls.py
+++ core/reports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import DashboardView
 
 urlpatterns = [
-
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    path("", DashboardView.as_view()),  # Root URL now points to dashboard
 ]
