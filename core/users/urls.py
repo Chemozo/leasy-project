0318b04188fef0cf40620d8149f25aa1cd5e9d72
Chemozo <chemalebo@gmail.com>
2025-05-14 21:34:06 -0600

    Aded logout to urls

--- core/users/urls.py
+++ core/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import CustomLoginView, CustomLogoutView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
 ]
